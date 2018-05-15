340e30171c0d429e616ac1e11d3ce2659719cd8f
gentacle <gentacle00@gmail.com>
2018-05-15 14:42:47 +0900

    구문추가

--- tutorial01/react01/src/features/event/reducers.js
+++ tutorial01/react01/src/features/event/reducers.js
@@ -3,18 +3,23 @@ import { handleActions } from "redux-actions";
 import axios from "axios";
 //액션타입 불러오기
 
+
+//1.타입생성단
 export const types = {
   // 비동기 처리시 준비, 성공, 실패 단계로 나누어서 처리
+  //기본 초기화구문
   SET_TITLE_STRING : "EVENT/SET_TITLE_STRING",
 
   GET_TITIL_POST : "EVENT/GET_TITIL_POST",
 
+  //비동기처리문
   GET_TITIL_POST_PENDING : "EVENT/GET_TITIL_POST_PENDING",
   GET_TITIL_POST_SUCCESS : "EVENT/GET_TITIL_POST_SUCCESS",
   GET_TITIL_POST_FAILURE : "EVENT/GET_TITIL_POST_FAILURE"
 
 };
 
+//2.액션생성단
 export const actions = {
     setTitleString : (id=1) =>(
       {
@@ -24,6 +29,20 @@ export const actions = {
   )
 };
 
+
+//초기 상태값 설정. 나중에 combineReducers로 묶어서 사용함
+//말그대로 이니셜상태를 정의하는구문.
+const initialState = {
+  title:"hello !!",
+  pending:false,
+  error:false
+};
+
+
+/* <Anthor FnList>-------------------------------------------------- */
+
+//(비동기)getTitle함수정의. 인자로 id를 받음.
+//axios라이브러리를 이용, 특정위치의 json형식의 파일을 읽어옴.
 async function getTitle (id){
   return await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
   .then(res =>{
@@ -37,12 +56,8 @@ async function getTitle (id){
   })
 }
 
-//초기 상태값 설정. 나중에 combineReducers로 묶어서 사용함
-const initialState = {
-  title:"hello !!",
-  pending:false,
-  error:false
-};
+
+/* <Reducer FnList>------------------------------------------------ */
 
 // 액션타입에 따라 데이터 반영
 export default handleActions(
--- tutorial01/react01/src/rootReducer.js
+++ tutorial01/react01/src/rootReducer.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import event from './features/event/reducers';
+
 export default combineReducers(
     {
       // 리듀서 넣기
